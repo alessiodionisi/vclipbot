9f93e74b004c7e5a3507fcbfaafd02ce568ca861
ga2arch <gabriele.carrettoni@gmail.com>
2020-07-23 21:55:33 +0200

    fix env var error

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ use crate::dal::yarn_api::YarnApiImpl;
 use crate::dao::clip_dao::ClipDaoImpl;
 use std::env;
 use std::sync::Arc;
+use log::{info};
 
 #[tokio::main]
 async fn main() {
     setup_logging();
 
-    let token = env::var("TELEGRAM_API_KEY").unwrap_or("TELEGRAM_API_KEY not found".to_string());
+    let token = env::var("TELEGRAM_API_KEY").expect("TELEGRAM_API_KEY not found");
     let yarn_api = Arc::new(YarnApiImpl::new("https://getyarn.io".to_string()));
     let clip_dao = Arc::new(ClipDaoImpl::new(yarn_api));
     let clip_bot = Arc::new(TelegramClipBot::new(token, clip_dao));
 
+    info!("starting clip bot");
     telegram_bot::run(100, clip_bot).await;
 }
 
